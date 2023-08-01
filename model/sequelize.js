647a404ec93ce198ef4c204e400de603526f3d8f
vaibhav parmar <84400764+vaibhav519@users.noreply.github.com>
2023-08-02 01:49:43 +0530

    Update sequelize.js

--- model/sequelize.js
+++ model/sequelize.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: '127.0.0.1',
+  host: 'localhost',
   dialect: 'mysql',
 });
 
