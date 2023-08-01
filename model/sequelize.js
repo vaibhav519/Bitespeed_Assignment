d8d7c3e946020714ec6fe1980417342e529434ae
vaibhav parmar <84400764+vaibhav519@users.noreply.github.com>
2023-08-02 03:18:42 +0530

    Update sequelize.js

--- model/sequelize.js
+++ model/sequelize.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(process.env.CC_MYSQL_ADDON_DB, process.env.CC_MYSQL_ADDON_USER, process.env.CC_MYSQL_ADDON_PASSWORD, {
+    host: process.env.CC_MYSQL_ADDON_HOST,
+    port: process.env.CC_MYSQL_ADDON_PORT,
+    dialect: 'mysql',
+  });
 
 module.exports = sequelize;
